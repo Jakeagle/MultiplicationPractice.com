da163d579053e25adc430874274f24a23c4e311d
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2022-10-25 20:04:28 -0500

    Initial file upload
    Initial upload of files

---
+++ index.js
@@ -0,0 +1,42 @@
+var max;
+var min;
+var valueOne;
+var valueTwo;
+var result;
+var playerAnswer;
+
+
+function beginChallenge() {
+  var max = 10;
+  var min = 1;
+  valueOne = Math.floor(Math.random() * (max - min + 1)) + min;
+  valueTwo = Math.floor(Math.random() * (max - min + 1)) + min;
+  document.getElementById("valOne").innerHTML = valueOne;
+  document.getElementById("valTwo").innerHTML = valueTwo;
+  result = valueOne * valueTwo;
+  console.log(result);
+ 
+
+}
+
+function Multiplier() {
+   
+    
+}
+
+
+function checkAnswer() {
+
+    playerAnswer = document.getElementById("answerValue").value;
+    console.log(playerAnswer);
+
+    if (playerAnswer == result) {
+        console.log("active");
+        beginChallenge();
+  }
+  else if (playerAnswer != result) {
+      console.log("incorrect");
+     alert("Try Again");
+   }
+
+}
