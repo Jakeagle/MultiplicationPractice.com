239b5d9e3d36f0ff9bd6f940168f2e70a98102f5
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2022-11-06 20:37:24 -0600

    Update index.js
    Added code to send alerts when min and max have no inputs, and when the min input is greater than the max input

--- index.js
+++ index.js
@@ -2,21 +2,46 @@ var valueOne;
 var valueTwo;
 var result;
 var playerAnswer;
+let max;
+let min;
+let noNumbers = false;
+
+
 
 
 function beginChallenge() {
-    var max = parseInt(document.getElementById("Max-Input").value);
-    var min = parseInt(document.getElementById("Min-Input").value);
 
-    valueOne = Math.floor(Math.random() * (max - min + 1)) + min;
-    valueTwo = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById("valOne").innerHTML = valueOne;
-    document.getElementById("valTwo").innerHTML = valueTwo;
-    result = valueOne * valueTwo;
-    console.log(result);
+    max = parseInt(document.getElementById("Max-Input").value);
+    min = parseInt(document.getElementById("Min-Input").value);
 
+    if (min >= max) {
+        alert(`Your min number must be smaller than your max`);
+        document.getElementById("Max-Input").value = null;
+        document.getElementById("Min-Input").value = null;
+        document.getElementById("valOne").innerHTML = null;
+        document.getElementById("valTwo").innerHTML = null;
+    }
+    if ((document.getElementById("Max-Input").value === "" || (document.getElementById("Min-Input").value === ""))) {
+        alert(`You must put numbers in both the Min and Max box to begin.`);
+        document.getElementById("valOne").innerHTML = null;
+        document.getElementById("valTwo").innerHTML = null;
+    }
+    else {
+        valueOne = Math.floor(Math.random() * (max - min + 1)) + min;
+        valueTwo = Math.floor(Math.random() * (max - min + 1)) + min;
+        document.getElementById("valOne").innerHTML = valueOne;
+        document.getElementById("valTwo").innerHTML = valueTwo;
+    }
+   result = valueOne * valueTwo;
+   
+ 
+    
 
 }
+
+
+
+
 
 function answerClick() {
     if (event.key === "Enter") {
@@ -25,15 +50,15 @@ function answerClick() {
 }
 
 function resetValues() {
-    var max = "0 ";
     var min = " 0";
     var max = " 0";
-    var min = "0 ";
     var valueOne = "0 ";
     var valueTwo = " 0";
     var result = "0 ";
     document.getElementById("valOne").innerHTML = "0 ";
     document.getElementById("valTwo").innerHTML = "0 ";
+    document.getElementById("Max-Input").value = null;
+    document.getElementById("Min-Input").value = null;
 }
 
 function checkAnswer() {
